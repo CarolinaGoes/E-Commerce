300b6d9087d88939fb7d381706c277c7416e9e36
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-03-31 19:38:33 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let header = document.querySelector("body");
 document.createElement("header");
 document.body.appendChild(header);
+const headerSections = ["Logo","imagem","Login","Carrinho" ];
+
+headerSections.map((el,chave) => {
+    let sectionElement = document.createElement("section");
+    sectionElement.innerHTML = section;
+    header.appendChild(sectionElement);
+}
